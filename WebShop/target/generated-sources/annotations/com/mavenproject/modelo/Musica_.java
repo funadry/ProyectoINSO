7c4d4e300faff74ce521594bc256d81fd22d795f
DSobrH <dsobrh00@estudiantes.unileon.es>
2018-05-04 20:12:34 +0200

    Sesión2_David
    Comenzando a implementar la vista del Login. Creado un template para Login y registro (tal vez se elimine este template).

---
+++ WebShop/target/generated-sources/annotations/com/mavenproject/modelo/Musica_.java
@@ -0,0 +1,17 @@
+package com.mavenproject.modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-03T19:23:25")
+@StaticMetamodel(Musica.class)
+public class Musica_ { 
+
+    public static volatile SingularAttribute<Musica, String> descripcion;
+    public static volatile SingularAttribute<Musica, Integer> idCancion;
+    public static volatile SingularAttribute<Musica, Double> precio;
+    public static volatile SingularAttribute<Musica, String> etiquetas;
+    public static volatile SingularAttribute<Musica, String> tituloCancion;
+
+}
