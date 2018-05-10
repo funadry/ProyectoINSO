853639e6c93582f7f929cad62b9a8623b8ecdc77
funadry <36886989+funadry@users.noreply.github.com>
2018-05-10 19:44:00 +0200

    Menu Mapeo y Facade
    Menu, libros y arte con facade mapeo de base de datos, etc.

---
+++ WebShop/target/generated-sources/annotations/com/mavenproject/modelo/Arte_.java
@@ -0,0 +1,16 @@
+package com.mavenproject.modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-09T16:38:12")
+@StaticMetamodel(Arte.class)
+public class Arte_ { 
+
+    public static volatile SingularAttribute<Arte, String> Descripcion;
+    public static volatile SingularAttribute<Arte, Integer> id;
+    public static volatile SingularAttribute<Arte, String> Titulo;
+    public static volatile SingularAttribute<Arte, Double> Precio;
+
+}
